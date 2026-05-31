9a11c9dc126546f457b357b7bcf70face2cbd18d
trkhanhlinh382 <trkhanhlinh382@gmail.com>
2026-05-31 04:55:46 -0700

    a

--- client/src/env.d.ts
+++ client/src/env.d.ts
@@ -4,6 +4,11 @@ interface ImportMetaEnv {
   readonly VITE_APP_URL?: string
   readonly VITE_TELEGRAM_BOT_URL?: string
   readonly VITE_TREASURY_ADDRESS?: string
+  readonly VITE_BIRD_REWARD_CONTRACT?: string
+  readonly VITE_GAME_PIPE_GAP?: string
+  readonly VITE_GAME_PIPE_SPEED?: string
+  readonly VITE_GAME_FLAP_FORCE?: string
+  readonly VITE_GAME_GRAVITY?: string
 }
 
 interface ImportMeta {
